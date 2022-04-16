b922d1b1a8c13c6bae71ac8acdeff40d4e314a2f
Your Name <you@example.com>
2022-04-16 11:25:35 +0300

    Removed signal for Blog

--- cooking_world/main/signals.py
+++ cooking_world/main/signals.py
@@ -6,10 +6,9 @@ from django.conf import settings
 from django.db.models import signals
 from django.dispatch import receiver
 
-
-@receiver(signals.pre_delete, sender=Blog)
-def delete_blog_photo(instance, **kwargs):
-    db_blog = Blog.objects.get(id=instance.id)
-    blog_photo = db_blog.photo
-    photo_path = join(settings.MEDIA_ROOT, str(blog_photo))
-    os.remove(photo_path)
+# @receiver(signals.pre_delete, sender=Blog)
+# def delete_blog_photo(instance, **kwargs):
+#     db_blog = Blog.objects.get(id=instance.id)
+#     blog_photo = db_blog.photo
+#     photo_path = join(settings.MEDIA_ROOT, str(blog_photo))
+#     os.remove(photo_path)
